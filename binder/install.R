c20834659c4edde87e201c0ace81273ad9563465
Chris Kennedy <chrisken@gmail.com>
2022-09-06 18:08:16 -0400

    binder update

--- binder/install.R
+++ binder/install.R
@@ -1,20 +1,2 @@
- cran_packages =
-  c("bookdown",
-    "caret",
-    "dplyr",
-    "glmnet",
-    "ggplot2",
-    "pROC",
-    "ranger",
-    "rpart",
-    "rpart.plot",
-    "remotes",
-    "rio",
-    "SuperLearner",
-    "vip",
-    "xgboost",
-    "PCAmixdata", "pvclust", "mclust", 
-    "ape",
-    NULL)
-install.packages(cran_packages)
-remotes::install_github("ck37/ck37r")
+install.packages("renv")
+renv::restore()
